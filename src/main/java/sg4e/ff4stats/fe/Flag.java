8640aacc7669c271945bd6786bb77610c6defa19
sg4e <46875746+sg4e@users.noreply.github.com>
2019-01-30 19:24:55 -0500

    Add flag data This commit includes a dependency for a different CSV loader to inject data into POJOs. The rest of the project may eventually transition to use this library for simpler data loading.

---
+++ src/main/java/sg4e/ff4stats/fe/Flag.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright (C) 2019 sg4e
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package sg4e.ff4stats.fe;
+
+import com.google.common.base.Functions;
+import com.univocity.parsers.annotations.Parsed;
+import com.univocity.parsers.common.processor.BeanListProcessor;
+import com.univocity.parsers.csv.CsvParser;
+import com.univocity.parsers.csv.CsvParserSettings;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author sg4e
+ */
+public class Flag {
+    
+    @Parsed
+    private String name;
+    
+    @Parsed
+    private int offset;
+    
+    @Parsed
+    private int size;
+    
+    @Parsed
+    private int value;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getOffset() {
+        return offset;
+    }
+
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+    
+    private static final List<Flag> FLAG_SPEC;
+    private static final Map<String, Flag> NAMES_TO_FLAGS;
+    
+    static {
+        BeanListProcessor<Flag> flagProcessor = new BeanListProcessor<>(Flag.class);
+        CsvParserSettings parserSettings = new CsvParserSettings();
+        parserSettings.setProcessor(flagProcessor);
+        parserSettings.setHeaderExtractionEnabled(true);
+
+        CsvParser parser = new CsvParser(parserSettings);
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+	InputStream inputStream = classLoader.getResourceAsStream("fe/flags.csv");
+        List<Flag> flags = Collections.emptyList();
+        try {
+            Reader reader = new InputStreamReader(inputStream);
+            parser.parse(reader);
+            flags = Collections.unmodifiableList(flagProcessor.getBeans());
+        }
+        catch(Exception ex) {
+            System.err.println("Error loading flags.csv");
+            ex.printStackTrace();
+        }
+        FLAG_SPEC = flags;
+        NAMES_TO_FLAGS = Collections.unmodifiableMap(
+                FLAG_SPEC.stream().collect(Collectors.toMap(Flag::getName, Functions.identity())));
+    }
+    
+    public static List<Flag> getAllFlags() {
+        return FLAG_SPEC;
+    }
+    
+    public static Flag getFlagByName(String name) {
+        return NAMES_TO_FLAGS.get(name);
+    }
+    
+}
---
+++ src/test/java/sg4e/ff4stats/fe/FlagTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2019 sg4e
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package sg4e.ff4stats.fe;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author sg4e
+ */
+public class FlagTest {
+
+    @Test
+    public void testFlagCsvLoading() {
+        assertTrue(Flag.getAllFlags().size() > 30);
+    }
+    
+    @Test
+    public void testFlagMapping() {
+        Flag startRydia = Flag.getFlagByName("-startrydia");
+        //name,offset,size,value
+        //-startrydia,19,4,3
+        assertEquals(19, startRydia.getOffset());
+        assertEquals(4, startRydia.getSize());
+        assertEquals(3, startRydia.getValue());
+    }
+    
+}
